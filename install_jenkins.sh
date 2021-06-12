740d5ccf68e21a33adf28a14c920d5f4bb14064a
root <root@ip-172-31-46-113.ap-south-1.compute.internal>
2021-06-12 06:37:00 +0000

    updated install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -9,7 +9,7 @@ sudo apt install python3-pip -y > /tmp/install_pip3.out
 sudo pip3 install ansible > /tmp/install_ansible.out
 sudo pip3 install paramiko > /tmp/install_paramiko.out
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
-sudo apt install maven > /tmp/install_maven.out
+sudo apt install maven -y > /tmp/install_maven.out
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu focal stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y >> /tmp/aptupdate.out
